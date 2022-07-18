b55d782625eb4568e53352ef329edc2a1e4fb993
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2022-07-18 17:12:54 +0700

    .

--- src/actions/addNewBoost/index.tsx
+++ src/actions/addNewBoost/index.tsx
@@ -1,17 +1,20 @@
 import { useState } from 'react'
 import { Button, Col, Modal, Row } from 'antd'
 import { useAppRouter } from 'hooks/useAppRouter'
+import { useInitializeBooster } from 'hooks/actions/useInitializeBooster'
 
 const AddNewBoost = () => {
   const { pushHistory } = useAppRouter()
+  const { initializeBooster, loading } = useInitializeBooster()
   const [visibleCancelPopup, setVisibleCancelPopup] = useState(false)
-  const onCreateBooster = () => {}
 
   return (
     <Row>
       <Col>
         <Button onClick={() => setVisibleCancelPopup(true)}>Cancel</Button>
-        <Button onClick={onCreateBooster}>Add</Button>
+        <Button onClick={initializeBooster} loading={loading}>
+          Add
+        </Button>
       </Col>
       <Modal
         title="Are you sure you want to cancel?"
