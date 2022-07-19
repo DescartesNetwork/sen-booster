e6f28c31606db58938614b4e7371f1fbeb9ab527
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2022-07-19 14:11:15 +0700

    add dark, light css files

--- src/bootstrap.app.tsx
+++ src/bootstrap.app.tsx
@@ -6,6 +6,10 @@ import {
   MintProvider,
 } from '@sentre/senhub'
 
+import './static/styles/dark.less'
+import './static/styles/light.less'
+import './static/styles/index.less'
+
 import View from 'view'
 
 import model from 'model'
@@ -15,9 +19,11 @@ const {
   manifest: { appId },
 } = configs
 
+console.log('app ID: ', appId)
+
 export const Page = () => {
   return (
-    <UIProvider appId={appId} antd>
+    <UIProvider appId={appId} antd={{ prefixCls: appId }}>
       <WalletProvider>
         <AccountProvider>
           <MintProvider>
