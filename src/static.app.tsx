777e88c1c7b5e5500d19593b5f721344eb3be463
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2022-08-03 13:11:13 +0700

    update Readme file and panel images

--- src/static.app.tsx
+++ src/static.app.tsx
@@ -1,5 +1,6 @@
-import panel from 'static/images/panel.png'
+import panel1 from 'static/images/panel_1.png'
+import panel2 from 'static/images/panel_2.png'
 
-export const panels = [panel]
+export const panels = [panel1, panel2]
 export { default as logo } from 'static/images/logo.svg'
 export { default as readme } from 'static/docs/README.md'
